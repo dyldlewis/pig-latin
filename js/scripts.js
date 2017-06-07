b9047405f97dea0f9d994b6b0a4921c5bfbc6b0f
Benjamin Hampikian and Dylan Lewis <benjamin@>
2017-06-07 13:29:53 -0700

    make it work

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,27 @@ $(document).ready(function() {
     event.preventDefault();
 
     var pigLatin = function(sentence) {
-      return false;
-    }
+      for (word = 0; word <= sentence.length; word += 1) {
+        if (sentence[word].charAt(0) === "q" && sentence[word].charAt(1) === "u") {
+          $("#output").append(sentence[word].substr(2, sentence[word].length) + sentence[word].charAt(0) + sentence[word].charAt(1) + "ay" + " ")
+        } else if (sentence[word].charAt(0) === "a" || sentence[word].charAt(0) === "e" || sentence[word].charAt(0) === "i" || sentence[word].charAt(0) === "o" || sentence[word].charAt(0) === "u") {
+              $("#output").append(sentence[word] + "ay" + " ")
+        } else if (sentence[word].charAt(0) != "a" || sentence[word].charAt(0) != "e" || sentence[word].charAt(0) != "i" || sentence[word].charAt(0) != "o" || sentence[word].charAt(0) != "u") {
+          $("#output").append(sentence[word].substr(1, sentence[word].length) + sentence[word].charAt(0) + "ay" + " ")
+        }
+        }
+      }
 
 
 
 
 
-    var sentence = $("input#sentence").val();
+
+    var vowels = ["a", "e", "i", "o", "u"];
+    var sentence = $("input#sentence").val().toLowerCase().split(" ");
+    $("#output").show();
     $("#output").text(pigLatin(sentence));
     var result = pigLatin(sentence)
+    console.log(sentence)
   });
 });
